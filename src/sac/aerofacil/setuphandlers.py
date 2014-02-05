ecb7b0e52f1849e42f5e81f1968378c19518813d
Davi Lima <davilima6@gmail.com>
2014-02-05 10:39:07 -0300

    Ajusta aplicação das configurações de instalação

--- src/sac/aerofacil/setuphandlers.py
+++ src/sac/aerofacil/setuphandlers.py
@@ -2,56 +2,58 @@
 import logging
 from zope.component.hooks import getSite
 from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.WorkflowCore import WorkflowException
+from Products.CMFCore.interfaces import IContentish
+from Products.Five.utilities.marker import mark
+from interfaces import IAplicativo, IAeroporto, IGuiaDoPassageiro
 
-
-def doUpgrades(context):
+def do_upgrades(context):
     """ If exists, run migrations
     """
     if context.readDataFile('sac.aerofacil.txt') is None:
         return
-    logger = logging.getLogger('sac.aerofacil')
     portal = getSite()
     _delete_example_content(portal)
+    _exclude_from_nav(portal)
+    _apply_interfaces(portal)
     _publish_structure(portal)
-    _excludeFromNav(portal)
-    _applyInterfaces(portal)
-    _updateCatalog(portal)
-    _setDefaultPage(portal)
+    _update_catalog(portal)
+    _set_layout(portal)
 
 def _delete_example_content(portal):
-    all_content = portal.portal_catalog()
-    if all_content:
-        expected = [
-            'front-page',
-            'news',
-            'aggregator',
-            'events',
-            'aggregator',
-            'Members'
-        ]
-        if not [i.id for i in all_content] == expected:
-            return
-        to_delete = ['front-page', 'news', 'events']
-        portal.manage_delObjects(to_delete)
+    to_delete = ['front-page', 'news', 'events']
+    to_delete = [i for i in to_delete if hasattr(portal, i)]
+    portal.manage_delObjects(to_delete)
+
+def _exclude_from_nav(portal):
+    to_exclude = ['destaques', 'Members']
+    to_exclude = [portal[i] for i in to_exclude if hasattr(portal, i)]
+    for obj in to_exclude:
+        obj.setExcludeFromNav(True)
+
+def _apply_interfaces(portal):
+    to_apply = {'aeroportos-brasileiros': IAeroporto,
+                'aplicativos-e-ferramentas': IAplicativo,
+                'guia-do-passageiro': IGuiaDoPassageiro
+    }
+    to_apply = {k: v for k, v in to_apply.iteritems() if hasattr(portal, k)}
+    for i in to_apply:
+        mark(portal[i], to_apply[i])
 
 def _publish_structure(portal):
-    pass
+    wtool = getToolByName(portal, 'portal_workflow')
+    logger = logging.getLogger('sac.aerofacil _publish_structure')
+    to_publish = [i for i in portal.keys() if IContentish.providedBy(portal[i])]
+    for i in to_publish:
+        try:
+            wtool.doActionFor(portal[i], "publish")
+        except WorkflowException:
+            logger.info("Could not publish: %s. Already published?" % i)
+            pass
 
-def _excludeFromNav(portal):
-    to_exclude = ['destaques', 'Members']
-    pass
-
-def _applyInterfaces(portal):
-    to_apply = ['home',
-                'aeroportos-brasileiros',
-                'aplicativos-e-ferramentas',
-                'guia-do-viajante'
-    ]
-    pass
-
-def _updateCatalog(portal, clear=True):
-    logger = portal.getLogger('sac.aerofacil updateCatalog')
-    logger.info('Updating catalog (with clear=%s) so items in profiles/default/structure are indexed...' % clear)
+def _update_catalog(portal, clear=True):
+    logger = logging.getLogger('sac.aerofacil _update_catalog')
+    logger.info('Updating catalog so items in profiles/default/structure are indexed...')
     catalog = getToolByName(portal, 'portal_catalog')
     err = catalog.refreshCatalog(clear=clear)
     if not err:
@@ -59,16 +61,8 @@ def _updateCatalog(portal, clear=True):
     else:
         logger.warn('Could not update catalog.')
 
-def _setDefaultPage(portal):
-    if portal.readDataFile('sac.aerofacil.txt') is None:
-        return
-    logger = portal.getLogger('sac.aerofacil setDefaultPage')
-    obj = 'home'
-    if obj in portal:
-        obj = portal[obj]
-        logger.info('Configurando item padrão para %s...' % obj)
-        obj.saveDefaultPage('%s_view' % obj)
-        logger.info('Configurando visão padrão para %s...' % obj)
-        obj.setLayout(pd)
-        logger.info('...Feito.')
-        return
+def _set_layout(portal):
+    logger = logging.getLogger('sac.aerofacil _set_default_page')
+    logger.info('Configurando visão padrão para %s...' % portal)
+    portal.setLayout('folder_tabular_view')
+    logger.info('...Feito.')
