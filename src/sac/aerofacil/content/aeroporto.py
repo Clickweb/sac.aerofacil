dbae1b26135c04c28569cfb1b094588cd308db63
Davi Lima <davilima6@gmail.com>
2014-02-07 08:42:18 -0300

    Correção nas browser views das internas

--- src/sac/aerofacil/content/aeroporto.py
+++ src/sac/aerofacil/content/aeroporto.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from five import grok
 from plone.supermodel import model
+from sac.aerofacil import interfaces
 
 grok.templatedir('templates')
 
@@ -17,3 +18,4 @@ class Aeroporto(grok.View):
     grok.name('view')
     grok.context(IAeroporto)
     grok.require('zope2.View')
+    grok.implements(interfaces.IAeroporto)
--- src/sac/aerofacil/content/aplicativo.py
+++ src/sac/aerofacil/content/aplicativo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from five import grok
 from plone.supermodel import model
+from sac.aerofacil import interfaces
 
 grok.templatedir('templates')
 
@@ -17,3 +18,4 @@ class Aplicativo(grok.View):
     grok.name('view')
     grok.context(IAplicativo)
     grok.require('zope2.View')
+    grok.implements(interfaces.IAplicativo)
