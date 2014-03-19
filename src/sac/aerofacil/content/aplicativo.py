8dd2e5a2ee5aa542aed62f47af1c3732f3d1cb3e
Davi Lima <davilima6@gmail.com>
2014-03-19 13:56:44 -0300

    Implementa métodos image_thumb e tag para exibição do conteúdo no (antigo) template folder_summary_view

--- src/sac/aerofacil/content/aplicativo.py
+++ src/sac/aerofacil/content/aplicativo.py
@@ -6,6 +6,11 @@ from sac.aerofacil import interfaces
 grok.templatedir('templates')
 
 
+def has_image(content):
+    image = content.image
+    return (image and image.getSize())
+
+
 class IAplicativo(model.Schema):
     """Um aplicativo contendo título, chamada, corpo de texto e campos
     para URLs de download.
@@ -19,3 +24,21 @@ class Aplicativo(grok.View):
     grok.context(IAplicativo)
     grok.require('zope2.View')
     grok.implements(interfaces.IAplicativo)
+
+    def image_thumb(self):
+        ''' Return a thumbnail '''
+        if not has_image(self):
+            return None
+        view = self.unrestrictedTraverse('@@images')
+        return view.scale(fieldname='image',
+                          scale='thumb').index_html()
+
+    def tag(self, scale='thumb', css_class='tileImage', **kw):
+        ''' Return a tag to the image '''
+        if not (has_image(self)):
+            return ''
+        view = self.unrestrictedTraverse('@@images')
+        return view.tag(fieldname='image',
+                        scale=scale,
+                        css_class=css_class,
+                        **kw)
